0cbe86142d608bb7d049ff2f58452d7cf8215b61
김준성 <70435510+kimjunsung04@users.noreply.github.com>
2021-10-21 21:49:11 +0900

    🎉 공부시작!

---
+++ 1. 정렬알고리즘/선택정렬.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	int min, index, temp;
+	int array[10] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+	for(int i=0;i<10;i++){
+		min = 9999;
+		for(int j=i;j<10;j++){
+			if(min>array[j]){
+				min = array[j];
+				index = j;
+			}
+		}
+		temp = array[i];
+		array[i] = array[index];
+		array[index] = temp;
+	}
+	
+	for(int i=0;i<10;i++){
+		printf("%d ", array[i]);
+	}
+}
+
+// 선택정렬의 시간복잡도 : O(N^2) 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # algorithm_learning
 알고리즘 공부하면서 작성한 소스 업로드
+
+[강좌정보](https://www.youtube.com/playlist?list=PLRx0vPvlEmdDHxCvAQS1_6XV4deOwfVrz)
